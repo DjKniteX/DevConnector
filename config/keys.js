2a8288b0d52269492a5638e0363febd122c1b743
Nikki X <djknitex@outlook.com>
2018-11-10 23:00:37 -0800

    copied code from Traversy sample; JWT now works

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mongoURI: 'mongodb://admin:admin123@ds151602.mlab.com:51602/scratch-nikkix'
+    mongoURI: 'mongodb://admin:admin123@ds151602.mlab.com:51602/scratch-nikkix',
+    secretOrKey: 'secret'
 }
 
 // module.exports makes the file avaiable outside of this code
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys')
 
 // Load User Model
 const User = require('../../models/User');
@@ -11,17 +13,23 @@ const User = require('../../models/User');
 // @desc    Tests user route
 // @access  Public
 
-router.get('/test', (req, res) => res.json({msg: "Users Works"}));
+router.get('/test', (req, res) => res.json({
+    msg: "Users Works"
+}));
 
 // @route   GET api/users/register
 // @desc    Register USER
 // @access  Public
 
 router.post('/register', (req, res) => {
-    User.findOne({ email: req.body.email})
-        .then( user => {
-            if(user) {
-                return res.status(400).json({email: 'Email already exists'})
+    User.findOne({
+            email: req.body.email
+        })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({
+                    email: 'Email already exists'
+                })
             } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200', //size of avatar
@@ -37,10 +45,10 @@ router.post('/register', (req, res) => {
                 });
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if(err) throw err;
+                        if (err) throw err;
                         newUser.password = hash;
                         newUser.save()
-                            .then( user => res.json(user))
+                            .then(user => res.json(user))
                             .catch(err => console.log(err));
                     })
                 })
@@ -48,7 +56,55 @@ router.post('/register', (req, res) => {
         })
 });
 
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({
+            email
+        })
+        .then(user => {
+            // check for user
+            if (!user) {
+                return res.status(404).json({
+                    email: 'User not found'
+                });
+            }
+
+            // Check Password
+            bcrypt.compare(password, user.password).then(isMatch => {
+                if (isMatch) {
+                    // User Matched
+                    const payload = {
+                        id: user.id,
+                        name: user.name,
+                        avatar: user.avatar
+                    }; // Create JWT Payload
+
+                    // Sign Token
+                    jwt.sign(
+                        payload,
+                        keys.secretOrKey, {
+                            expiresIn: 3600
+                        },
+                        (err, token) => {
+                            res.json({
+                                success: true,
+                                token: 'Bearer ' + token
+                            });
+                        }
+                    );
+                } else {
+                    errors.password = 'Password incorrect';
+                    return res.status(400).json(errors);
+                }
+            });
+        });
+});
 
 
 module.exports = router;
-

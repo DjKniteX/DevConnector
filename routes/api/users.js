ee0e105ef824937b34eab2e822fe32e2c22bb38e
Nikki X <djknitex@outlook.com>
2018-11-11 16:49:42 -0800

    fixed validation to postman using brad's code

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User Model
 const User = require('../../models/User');
@@ -34,20 +35,16 @@ router.post('/register', (req, res) => {
     }
 
 
-    User.findOne({
-            email: req.body.email
-        })
-        .then(user => {
-            if (user) {
-                return res.status(400).json({
-                    email: 'Email already exists'
-                })
-            } else {
-                const avatar = gravatar.url(req.body.email, {
-                    s: '200', //size of avatar
-                    r: 'pg', // rating
-                    d: 'mm' // default
-                });
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+          errors.email = 'Email already exists';
+          return res.status(400).json(errors);
+        } else {
+          const avatar = gravatar.url(req.body.email, {
+            s: '200', // Size
+            r: 'pg', // Rating
+            d: 'mm' // Default
+          });
 
                 const newUser = new User({
                     name: req.body.name,
@@ -72,52 +69,50 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+  
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+  
     const email = req.body.email;
     const password = req.body.password;
-
+  
     // Find user by email
-    User.findOne({
-            email
-        })
-        .then(user => {
-            // check for user
-            if (!user) {
-                return res.status(404).json({
-                    email: 'User not found'
-                });
+    User.findOne({ email }).then(user => {
+      // Check for user
+      if (!user) {
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
+      }
+  
+      // Check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User Matched
+          const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+  
+          // Sign Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
             }
-
-            // Check Password
-            bcrypt.compare(password, user.password).then(isMatch => {
-                if (isMatch) {
-                    // User Matched
-                    const payload = {
-                        id: user.id,
-                        name: user.name,
-                        avatar: user.avatar
-                    }; // Create JWT Payload
-
-                    // Sign Token
-                    jwt.sign(
-                        payload,
-                        keys.secretOrKey, {
-                            expiresIn: 3600
-                        },
-                        (err, token) => {
-                            res.json({
-                                success: true,
-                                token: 'Bearer ' + token
-                            });
-                        }
-                    );
-                } else {
-                    errors.password = 'Password incorrect';
-                    return res.status(400).json(errors);
-                }
-            });
-        });
-});
-
+          );
+        } else {
+          errors.password = 'Password incorrect';
+          return res.status(400).json(errors);
+        }
+      });
+    });
+  });
+  
 // @route   GET api/users/current
 // @desc    Return current user
 // @access  Private

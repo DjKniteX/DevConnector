f60894c735230ce328f445a947054a4d42f369e5
Nikki X <djknitex@outlook.com>
2018-11-18 15:00:27 -0800

    finished video 39: Set up basic Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 // Dependecies
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import { Provider } from 'react-redux';
+import store  from './store';
+
+
+
 
 // Components
 import Navbar from './components/layout/Navbar';  
@@ -10,9 +14,14 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
+import './App.css';
+
+
+
 class App extends Component {
   render() {
     return (
+      <Provider store = { store }>
       <Router>
       <div className="App">
       <Navbar />
@@ -23,8 +32,9 @@ class App extends Component {
        
        </div>
       <Footer />
-      </div>
+      </div> 
       </Router>
+      </Provider>
     );
   }
 }

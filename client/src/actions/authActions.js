7dba52846d35fd72eef8d20f28775b2fd18aa778
Nikki X <djknitex@outlook.com>
2018-11-18 21:00:00 -0800

    finished 42: Redux for login setup

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 // Register User
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utilis/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
 import axios from 'axios';
 
 
@@ -14,3 +17,32 @@ export const registerUser = (userData, history) => dispatch => {
     
 };
 
+// Login - Get User Token
+export const loginUser = (userData) => dispatch => {
+    axios.post('/api/users/login', userData)
+    .then(res => {
+        // Save to localstorage
+        const { token } = res.data;
+        // Set token to ls
+        localStorage.setItem('jwtToken', token);
+        // Set token to Auth header
+        setAuthToken(token);
+        // Decode token to get user data
+        const decoded = jwt_decode(token);
+        // Set current user
+        dispatch(setCurrentUser(decoded));
+
+    })
+    .catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }));
+};
+
+// set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}

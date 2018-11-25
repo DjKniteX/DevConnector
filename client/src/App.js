3444f41274376bbea464579e5985c24b53fb9ad5
Nikki X <djknitex@outlook.com>
2018-11-24 23:29:49 -0800

    finished video 47 & 48: Added spinner and private routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 // Dependecies
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store  from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utilis/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
 import { clearCurrentProfile } from './actions/profileActions';
-
+import PrivateRoute from './components/common/PrivateRoute';
 
 
 
@@ -56,7 +56,9 @@ class App extends Component {
        <div className="container">
        <Route exact path="/register" component={ Register } />
        <Route exact path="/login" component={ Login } />
-       <Route exact path="/dashboard" component={ Dashboard } />
+       <Switch>
+       <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+       </Switch>
        
        </div>
       <Footer />

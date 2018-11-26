b187d356297d1a57a7e519a6626a59ae154f31f8
Nikki X <djknitex@outlook.com>
2018-11-25 21:12:07 -0800

    finished video 51: Added select list group

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -1,35 +1,28 @@
-import React from 'react'
+import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const SelectListGroup = ({
-  name,
-  value,
-  error,
-  info,
-  onChange,
-  options
-}) => {
+const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
   const selectOptions = options.map(option => (
     <option key={option.label} value={option.value}>
       {option.label}
     </option>
-
-  ))
-
+  ));
   return (
     <div className="form-group">
-    <select className={classnames('form-control form-control-lg', {
-      'is-invalid': error
-    })} 
-    name={name}
-    value={value} 
-    onChange={onChange}>
-    </select>
-    {info && <small className="form-text text-muted">{info}</small>}
-    {error && (
-      <div className="invalid-feedback">{error}</div>)}
-  </div>
+      <select
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
+        })}
+        name={name}
+        value={value}
+        onChange={onChange}
+      >
+        {selectOptions}
+      </select>
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
   );
 };
 
@@ -40,7 +33,6 @@ SelectListGroup.propTypes = {
   error: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   options: PropTypes.array.isRequired
-}
+};
 
-
-export default SelectListGroup;  
+export default SelectListGroup;

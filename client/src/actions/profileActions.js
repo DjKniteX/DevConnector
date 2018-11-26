0832abe81737d7b9d6183b275d42e0e3c7b4067d
Nikki X <djknitex@outlook.com>
2018-11-25 21:35:58 -0800

    added delete account to profile action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types.js';
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER} from './types.js';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -41,6 +41,25 @@ export const createProfile = (profileData, history) => dispatch => {
       })
     );
 };
+
+// Delete Account
+export const deleteAccount = () => dispatch => {
+  if(window.confirm('Are you sure? This can NOT be undone!')) {
+    axios
+    .delete('/api/profile')
+    .then(res => 
+      dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+      })
+      ).catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+        )
+  }
+}
 
 
 // Clear profile

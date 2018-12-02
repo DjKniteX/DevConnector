f500b8d3181f46c6e9b76b9e78c1c7f440f1a10e
Nikki X <djknitex@outlook.com>
2018-12-01 18:36:41 -0800

    finished video 56 and added code to AddExperience

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -26,10 +26,26 @@ class AddExperience extends Component {
         this.onCheck = this.onCheck.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.errors) {
+        this.setState({ errors: nextProps.errors });
+      }
+    }
+  
     onSubmit(e) {
-        e.preventDefault();
-
-        console.log('submit');
+      e.preventDefault();
+  
+      const expData = {
+        company: this.state.company,
+        title: this.state.title,
+        location: this.state.location,
+        from: this.state.from,
+        to: this.state.to,
+        current: this.state.current,
+        description: this.state.description
+      };
+  
+      this.props.addExperience(expData, this.props.history);
     }
 
     onChange(e) {
@@ -137,6 +153,7 @@ class AddExperience extends Component {
   }
 
 AddExperience.propTypes = {
+    addExperience: PropTypes.func.isRequired,
     profile: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
 }
@@ -147,4 +164,4 @@ const mapStateToProps = state => ({
 });
 
 
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(mapStateToProps, {addExperience})(withRouter(AddExperience));

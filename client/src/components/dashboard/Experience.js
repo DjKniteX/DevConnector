8d98333928f2e8112a9cb80825555cf44ce06a9d
Nikki X <djknitex@outlook.com>
2018-12-01 21:01:13 -0800

    fixed education.js

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -12,27 +12,27 @@ onDeleteClick(id) {
 
   render() {
       const experience = this.props.experience.map(exp => (
-        <tr key={exp._id}>
-          <td>{exp.company}</td>
-          <td>{exp.title}</td>
-          <td>
-            <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
-            {exp.to === null ? (
-              ' Now'
-            ) : (
-              <Moment format="YYYY/MM/DD">{exp.to}</Moment>
-            )}
-          </td>
-          <td>
-            <button
-              onClick={this.onDeleteClick.bind(this, exp._id)}
-              className="btn btn-danger"
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      ));
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td>{exp.title}</td>
+        <td>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          {exp.to === null ? (
+            ' Now'
+          ) : (
+            <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+          )}
+        </td>
+        <td>
+          <button
+            onClick={this.onDeleteClick.bind(this, exp._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
     return (
       <div>
           <h4 className="mb-4">Experience Credentials</h4>

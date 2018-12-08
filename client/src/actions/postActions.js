02f16f2083ff23fb1abb2435cc9719c0754f1888
Nikki X <djknitex@outlook.com>
2018-12-08 10:32:01 -0800

    Added more post files, posts are beginning to work

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 import {
     ADD_POST,
-    GET_ERRORS
+    GET_ERRORS,
+    GET_POSTS,
+    POST_LOADING
 } from './types';
 
 
@@ -19,4 +21,28 @@ export const addPost = postData => dispatch => {
             type: GET_ERRORS,
             payload: err.response.data
         }))
+}
+
+// Get Post
+
+export const getPosts = () => dispatch => {
+    dispatch(setPostLoading());
+    axios
+        .get('/api/posts')
+        .then(res => dispatch({
+            type: GET_POSTS,
+            payload: res.data
+        })
+        )
+        .catch( err => dispatch({
+            type: GET_POSTS,
+            payload: null
+        }))
+}
+
+// Set Loading State
+export const setPostLoading = () => {
+    return {
+        type: POST_LOADING
+    }
 }
